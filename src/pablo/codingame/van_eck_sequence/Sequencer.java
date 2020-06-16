a73f014bbe52a4530f819e42f8ba7eb957fda873
Pablo Rausch <pablorausch@gmail.com>
2020-06-16 16:59:35 -0300

    final solution

--- src/pablo/codingame/van_eck_sequence/Sequencer.java
+++ src/pablo/codingame/van_eck_sequence/Sequencer.java
@@ -1,13 +1,34 @@
 package pablo.codingame.van_eck_sequence;
 
+import java.util.HashMap;
+
 public class Sequencer {
-    private int a1;
+    private Integer a1;
+    private HashMap<Integer, Integer> map;
 
     public Sequencer(int a1){
         this.a1 = a1;
+        this.map = new HashMap<>();
+        this.map.put(a1, 1);
     }
 
     public int getElement(int pos) {
-        return a1;
+        var currentPos = 1;
+        var currentVal = a1;
+
+        while(currentPos < pos) {
+            if (this.map.containsKey(currentVal)) {
+                var newVal = currentPos - this.map.get(currentVal);
+                this.map.replace(currentVal, currentPos);
+                currentVal = newVal;
+            } else {
+                this.map.put(currentVal, currentPos);
+                currentVal = 0;
+            }
+
+            currentPos++;
+        }
+
+        return currentVal;
     }
 }
--- src/pablo/codingame/van_eck_sequence/test/SequencerTest.java
+++ src/pablo/codingame/van_eck_sequence/test/SequencerTest.java
@@ -12,4 +12,22 @@ public class SequencerTest {
         var sut = new Sequencer(2);
         assertEquals(2, sut.getElement(1));
     }
+
+    @Test
+    public void secondElementIsZero() {
+        var sut = new Sequencer(2);
+        assertEquals(0, sut.getElement(2));
+    }
+
+    @Test
+    public void exampleThirdPos() {
+        var sut = new Sequencer(0);
+        assertEquals(1, sut.getElement(3));
+    }
+
+    @Test
+    public void aLittleLong() {
+        var sut = new Sequencer(1);
+        assertEquals(11, sut.getElement(58));
+    }
 }
